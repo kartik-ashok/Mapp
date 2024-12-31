457e1084b098773458c51b7a8845a4b0504e9369
kartikkatti100@gmail.com <kartikkatti100@gmail.com>
2024-12-31 11:45:15 +0530

    first commit

---
+++ src/main/java/com/mediqube/mediqube/MediqubeApplication.java
@@ -0,0 +1,13 @@
+package com.mediqube.mediqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediqubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediqubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediqube/mediqube/MediqubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediqube.mediqube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediqubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
